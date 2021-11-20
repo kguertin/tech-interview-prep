8d635bb8dbf81be9ca9f68d76834c6c1744dfa8b
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-11-19 23:56:13 -0500

    write demo instructions

---
+++ interview_questions/demo_question.js
@@ -0,0 +1,13 @@
+// Given two arrays, create a function that lets a user know (true/flase) weather these two arrays contain any common items.
+// For Example:
+// const array1 = ['a','b','c','x']
+// const array2 = ['z','y','i']
+// should return false
+// -------------------------------
+// const array1 = ['a','b','c','x']
+// const array2 = ['z','y','x']
+// should return true
+
+// function should:
+// take 2 parameters
+// return true or false
