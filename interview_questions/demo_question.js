d1ad7fa212147db5c4c63b6be8b884139b24e8de
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-11-21 21:40:12 -0500

    nested loops solution for match problem

--- interview_questions/demo_question.js
+++ interview_questions/demo_question.js
@@ -9,5 +9,27 @@
 // should return true
 
 // function should:
-// take 2 parameters
+// take 2 parameters - arrays
 // return true or false
+
+// Might want to ask how big the array might be.
+// ask whats more important - space complexity or time complexity
+
+// Nested for loops - O(n^2) - Brute force solution
+// explain why this isn't the best.
+
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'i'];
+
+function containsCommonItem(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(containsCommonItem(array1, array2));
