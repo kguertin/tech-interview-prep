1c48acc2c7f8117bf70be02bbeb589807a51510a
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-11-24 09:30:00 -0500

    reverse string

--- data_structures/arrays/reverse_string.js
+++ data_structures/arrays/reverse_string.js
@@ -1,6 +1,26 @@
 function reverseString(string) {
-  const arr = string.split("");
-  return arr;
+  if (!string || string.length < 2 || typeof string !== 'string') {
+    return 'Invalid input';
+  }
+
+  let newString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 }
 
-console.log(reverseString("Hi my name is kevin"));
+function reverseRefactor(string) {
+  if (!string || string.length < 2 || typeof string !== 'string') {
+    return 'Invalid input';
+  }
+
+  return string.split('').reverse().join('');
+}
+
+const modernReverse = (string) => [...string].reverse().join('');
+
+console.log(reverseString('Hi my name is kevin')); // O(n)
+console.log(reverseString(0666)); // O(n)
+console.log(reverseRefactor('Hi my name is kevin'));
+console.log(modernReverse('Hi my name is kevin.'));
