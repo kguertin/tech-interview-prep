7f75823ba7db78021cf2fb954ae0f264b7e58392
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-11-24 06:35:21 -0500

    intro to arrays

--- data_structures/array_intro.js
+++ data_structures/array_intro.js
@@ -1,1 +1,26 @@
-[];
+const strings = ["a", "b", "c", "d"];
+// 4 items = 16 bytes of storage
+
+// Get the third item from array in memory
+strings[2]; // O(1)
+
+// push - add to end of array
+strings.push("e"); // O(1)
+console.log(strings);
+
+// pop - remove from end of array
+strings.pop();
+console.log(strings);
+
+// unshift - add to beginning of array
+strings.unshift("x"); // O(n) - has to reassign the indexes
+console.log(strings);
+
+// splice // O(n)
+strings.splice(2, 0, "alien");
+console.log(strings);
+
+// lookup - O(1)
+// push - O(1)
+// insertion - O(n)
+// deletion - O(n)
