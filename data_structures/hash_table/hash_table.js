7a2dbc8ca9809a45d4fae11519bfc55c882251a3
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2021-12-01 10:02:03 -0500

    refactor hashtable and handle collision

--- data_structures/hash_table/hash_table.js
+++ data_structures/hash_table/hash_table.js
@@ -12,19 +12,33 @@ class HashTable {
   }
 
   set(key, value) {
-    const hash = this._hash(key);
-    this.data[Math.floor(Math.random() * this.data.length)] = [hash, value];
+    const address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
   }
 
   get(key) {
-    const hash = this._hash(key);
-    const item = this.data.find((i) => i && i[0] === hash);
-    return item ? item[1] : null;
+    // O(1)
+    const address = this._hash(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
   }
 }
 
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 54);
 myHashTable.get('grapes');
+console.log(myHashTable.get('grapes'));
 
 // implement get and set for this table class
